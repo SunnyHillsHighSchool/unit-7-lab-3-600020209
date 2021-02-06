1f7dfaae3b3c5d88d2823a46b6148a38d5cc76b3
Joon Kyo Kyo <600020209@fjuhsd.org>
2021-02-06 05:01:24 +0000

    Completed methods getListOfFactors and keepOnlyCompositeNumbers in ArrayListFunHouse

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -12,10 +12,37 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+  //Create array list factors composed of Integers
+  ArrayList<Integer> factors = new ArrayList<Integer>();
+  //Start for loop, initializing int iterator i with value 2, while i is less than number (parameter), and incrementing i by 1 each run:
+  for(int i = 2; i < number; i++){
+    //Start if statement; when the modulo of number and i is 0:
+    if(number % i == 0){
+      //Add the value of i to the array list factors
+      factors.add(i);
+    //End if
+    }
+  //End for
+  }
+  //Return array list factors
+  return factors;
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
  {
+   //Declare integer array list original
+   ArrayList<Integer> original = new ArrayList<Integer>();
+  //Start for loop, initializing int iterator i with value of size of array list minus 1, while i is greater than or equal to 0, and decreasing i by one each run:
+  for(int i = nums.size() - 1; i >= 0; i--){
+    //Put value returned by getListOfFactors when passing the element in array list nums at position i through the method into original array
+    original = ArrayListFunHouse.getListOfFactors(nums.get(i));
+    //Start if statement: if the size of array list original is equal to 0:
+    if(original.size() == 0){
+      //Remove element at index i fo array list nums
+      nums.remove(i);
+    //End if
+    }
+  //End for
+  }
  }
 }
